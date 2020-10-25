459334d767b8a03bee4d45a6cf7dabb022d383a0
mgupta82 <mukeshsgupta@gmail.com>
2020-10-25 07:41:34 -0400

    Add test case for oauth server

--- src/test/java/com/auspost/postcode/controller/PostcodeControllerTest.java
+++ src/test/java/com/auspost/postcode/controller/PostcodeControllerTest.java
@@ -2,6 +2,10 @@ package com.auspost.postcode.controller;
 
 import com.auspost.postcode.dto.ErrorResponse;
 import com.auspost.postcode.repository.entity.Postcode;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,13 +20,28 @@ import org.springframework.test.util.AssertionErrors;
 @Sql(scripts= {"/scripts/test-search.sql"})
 public class PostcodeControllerTest {
 
+    private static final String ACCESS_TOKEN = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIzSVRITWstOF9VVzJzX3pVZjhZNXMzOHhlMkNLQXZ2X1hCU09RNDZoT3FnIn0.eyJleHAiOjE2MDM2MTcwMzIsImlhdCI6MTYwMzYxNjczMiwianRpIjoiY2NmZDdhMmYtYTkzOS00NWY3LTllMjUtZjMxOWRlMGNmMjA1IiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDcwL2F1dGgvcmVhbG1zL3Rlc3QiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiZmQ2Njc5NzMtNmY5OC00MDM4LWJjNWItMmFiZTYzNjBhYmIzIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoicG9zdGNvZGUtY2xpZW50Iiwic2Vzc2lvbl9zdGF0ZSI6IjU1MTg5YTE3LWIwNzUtNDIxMy05NDBhLWRjN2JiZTAwMjk5NCIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoicHJvZmlsZSBlbWFpbCIsImNsaWVudEhvc3QiOiIxNzIuMjYuMC4xIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJjbGllbnRJZCI6InBvc3Rjb2RlLWNsaWVudCIsInByZWZlcnJlZF91c2VybmFtZSI6InNlcnZpY2UtYWNjb3VudC1wb3N0Y29kZS1jbGllbnQiLCJjbGllbnRBZGRyZXNzIjoiMTcyLjI2LjAuMSJ9.ELeihF01h4DJYLp6Ht7pCuuZF4xfUFEX-ouX4U-C4eZFB0Iwro1-v3adzsv_MJuSL3J3k_b3wE6ECGTCrUrB0OQ3Y6HGu24W-5eRv4RLnV2MPcHV8_teSv6ryvlThGYYMaKi7O4RkIHlcQOkIMS4EPAkOTOlfKbKdyL1ZPzRxcN_Y13x8pqS3iWM1-zhtz7ptkQYsD4fSiFBnea3KdnOuTWwc9VE6Vlc2NqyiOfgLK_4KzpjcW17uKQ8VEfw4vHCIlamceXVgJFmOaewav9NcNNXo0qv_-T53NGsa9U0dmjaNOeeT2nDj7_HVHRBdM_rgd4kPLDXTF1f8jagnsg8Sw";
+
     @Autowired
     TestRestTemplate restTemplate;
+
+    private static WireMockServer wireMockServer = new WireMockServer(WireMockConfiguration.wireMockConfig().port(8089));
+
+    @BeforeAll
+    public static void setup() {
+        wireMockServer.start();
+    }
+
+    @AfterAll
+    public static void finish() {
+        wireMockServer.stop();
+    }
 
     @Test
     public void testAddPostcode() {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.set("content-type", MediaType.APPLICATION_JSON_VALUE);
+        httpHeaders.set("Authorization", ACCESS_TOKEN);
         Postcode postcode = Postcode.builder()
                 .code(3163)
                 .suburb("Carnegie")
@@ -40,6 +59,7 @@ public class PostcodeControllerTest {
     public void testGetPostcode() {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.set("content-type", MediaType.APPLICATION_JSON_VALUE);
+        httpHeaders.set("Authorization", ACCESS_TOKEN);
         HttpEntity request = new HttpEntity<>(null);
         ResponseEntity<Postcode> responseEntity = restTemplate.exchange("/postcode/details/3192", HttpMethod.GET,request,Postcode.class);
         AssertionErrors.assertEquals("Invalid Http Status",HttpStatus.OK,responseEntity.getStatusCode());
@@ -52,6 +72,7 @@ public class PostcodeControllerTest {
     public void testSearchSuburb() {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.set("content-type", MediaType.APPLICATION_JSON_VALUE);
+        httpHeaders.set("Authorization", ACCESS_TOKEN);
         HttpEntity request = new HttpEntity<>(null);
         ResponseEntity<Postcode[]> responseEntity = restTemplate.exchange("/postcode/suburb/cha", HttpMethod.GET,request, Postcode[].class);
         AssertionErrors.assertEquals("Invalid Http Status",HttpStatus.OK,responseEntity.getStatusCode());
@@ -62,6 +83,7 @@ public class PostcodeControllerTest {
     public void testErrorScenario() {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.set("content-type", MediaType.APPLICATION_JSON_VALUE);
+        httpHeaders.set("Authorization", ACCESS_TOKEN);
         Postcode postcode = Postcode.builder()
                 .code(3111)
                 .suburb(null)
